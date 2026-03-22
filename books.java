3368c910685fe9c97b8885d320c66faefc7d66e5
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-03-22 23:13:34 +0530

    intial build of my library app

---
+++ books.java
@@ -0,0 +1,30 @@
+package library_cli;
+public class books {
+    private int bookid;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean isAvailable;
+
+    public books( int bookid, String title, String author, String genre, boolean isAvailable ){
+        this.bookid = bookid;
+        this.title = title;
+        this.author= author;
+        this.genre = genre;
+        this.isAvailable = true;
+    }
+
+    //getters
+    public int getBookid() {return bookid;}
+    public String getTitle () {return title;}
+    public String getAuthor() {return author;}
+    public String getGenre() {return genre;}
+    public boolean getisAvailable(){return isAvailable;}
+
+    @Override /*toString() is a function which every object has and gets called whenever we System.out.print(object); which converts objects address to a string, 
+    but here we are overiding means we are overwriting this function to return a string which contains instance variables pf that object */
+    public String toString() {
+        return bookid+" | "+title+" | "+author+" | "+" | "+genre+" | "+isAvailable;
+    }
+
+}
