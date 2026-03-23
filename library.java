5c2e13e7de151bad2b78e53fbc4f63c9e6d22d43
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-03-23 23:40:06 +0530

    aded functions to 1.add a book and 2.display all the books function

--- library.java
+++ library.java
@@ -1,5 +1,20 @@
 package library_cli;
+import java.util.ArrayList;
 
 public class library {
-    private String book;
+    private ArrayList<book> books = new ArrayList<>();
+
+    public void addBook(book book){
+        books.add(book);
+        System.out.println(" Book has been added");
+    }
+
+    public void displayAllBooks(){
+        if (books.isEmpty()){
+            System.out.println("no books present");
+        }
+        for(book b: books){
+            System.out.println(b);
+        }
+    }
 }
